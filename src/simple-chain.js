2933f79780e0c3077d36c6561bb2fabc6ab98129
Art3m23 <76261475+Art3m23@users.noreply.github.com>
2021-01-21 18:14:28 +0300

    feat: add functions code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
-const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+let chainMaker = {
+  chain: [],
+  getLength() { 
+    return this.chain.length
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) { 
+    let link = '( )';
+    if (value !== undefined) {
+      link = '( ' + value + ' )'
+    }
+    this.chain.push(link)
+    return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) { 
+    if (position < 1 || position > this.getLength() || typeof(position) !== 'number') {
+      this.chain = []
+      throw Error
+    }
+    this.chain.splice(position - 1, 1)
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  finishChain() { 
+    let result = this.chain.join('~~')
+    this.chain = []
+    return result
   }
-};
-
+}
 module.exports = chainMaker;
