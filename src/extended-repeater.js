84a9ad1b1649ec0beebb1b5d5ac535491755e7a7
Art3m23 <76261475+Art3m23@users.noreply.github.com>
2021-01-21 18:06:21 +0300

    feat: add functions code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, opt) {
+  str = String(str)
+  let options = {
+    repeatTimes: ('repeatTimes' in opt && typeof(opt.repeatTimes) === 'number') ? opt.repeatTimes : 1, 
+    separator: 'separator' in opt ? opt.separator : '+', 
+    addition: 'addition' in opt ? String(opt.addition) : '', 
+    additionRepeatTimes: ('additionRepeatTimes' in opt && typeof(opt.additionRepeatTimes) === 'number') ? opt.additionRepeatTimes : 1, 
+    additionSeparator: 'additionSeparator' in opt ? opt.additionSeparator : '|' 
+  }
+  let str1 = ''
+  if (options.additionRepeatTimes > 0) {
+    str1 = options.addition;
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+      str1 += options.additionSeparator + options.addition
+    }
+  }
+  let s = str + str1
+  for (let i = 1; i < options.repeatTimes; i++) {
+      s += options.separator + str + str1
+  } 
+  return s
 };
   
