6438224845589a3d4969dfa71dad6920320c1ca5
Art3m23 <76261475+Art3m23@users.noreply.github.com>
2021-01-21 18:04:47 +0300

    feat: add functions code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ let s = false
+  if (Array.isArray(members)) {
+    let TeamName = members.reduce(function (teamName, i) {
+      if (typeof(i) === 'string') {
+        teamName.push(i.trim()[0].toUpperCase())
+      }
+      return teamName
+    }, [])
+    if (TeamName !== []) {
+      s = TeamName.sort().join('')
+    }
+  }
+  return s
 };
